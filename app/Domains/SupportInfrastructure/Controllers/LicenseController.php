571740b1cbf9265e6570ebae994b78c87f02c2a0
Zapata Carbonel Harold Andre <zapatacarbonel@gmail.com>
2025-10-22 01:02:00 -0500

    corrigiendo rutas de licenses en PUT

--- app/Domains/SupportInfrastructure/Controllers/LicenseController.php
+++ app/Domains/SupportInfrastructure/Controllers/LicenseController.php
@@ -5,7 +5,7 @@ namespace App\Domains\SupportInfrastructure\Controllers;
 use App\Http\Controllers\Controller;
 use App\Domains\SupportInfrastructure\Models\License;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Log;
 
 class LicenseController extends Controller{
     public function index(){
@@ -37,15 +37,34 @@ class LicenseController extends Controller{
     }
         public function show($id){
             return response()->json(
-                License::with(['employee', 'licenses'])->findOrFail($id)
+                License::with(['software', 'responsible'])->findOrFail($id)
             );
         }
 
+        #no me funciona este método update, no actualiza nada
         public function update(Request $request, $id)
         {
+            Log::info('Update request:', $request->all());
             $license = License::findOrFail($id);
-            $license->update($request->all());
+            
+            $data = $request->validate([
+            'software_name' => 'sometimes|string',
+            'license_key' => 'sometimes|string',
+            'license_type'=> 'nullable|string',
+            'provider'=> 'nullable|string',
+            'purchase_date' => 'nullable|date',
+            'expiration_date' => 'nullable|date',
+            'seats_total' => 'nullable|integer',
+            'seats_used' =>'nullable|integer',
+            'cost_annual' =>'nullable|numeric',
+            'status' =>'nullable|string',
+        #'responsible_id' =>'nullable|exists:employees,id',
+            'responsible_id' => 'nullable|integer',
+            'notes' =>'nullable|string'
+            ]);
+            $license->update($data);
             return response()->json($license);
+            Log::info('Updated license:', $license->toArray());
         }
 
         public function destroy($id){
