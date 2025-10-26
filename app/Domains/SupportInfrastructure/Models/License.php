10475b36707ef934a54275dbec506cab7a3445a7
CRUZ CASTILLO JHOAN ANTONI <116601508+Jhoan2-cpu@users.noreply.github.com>
2025-10-25 22:27:54 -0500

    fix:licenses

--- app/Domains/SupportInfrastructure/Models/License.php
+++ app/Domains/SupportInfrastructure/Models/License.php
@@ -8,7 +8,11 @@ class License extends Model{
     protected $fillable = ['software_name','license_key','license_type','provider','purchase_date',
     'expiration_date','seats_total','seats_used','cost_annual','status','responsible_id','notes',];
 
-    
+    protected $casts = [
+        'purchase_date' => 'date',
+        'expiration_date' => 'date',
+    ];
+
     public $timestamps = FALSE;
     public function software(){
         return $this->belongsTo(Software::class, 'software_id');
