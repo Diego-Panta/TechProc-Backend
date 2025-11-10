a67e08301be0a8cf13e062f2ed88634be2ff324c
Zapata Carbonel Harold Andre <zapatacarbonel@gmail.com>
2025-11-09 20:54:10 -0500

    upgrade: create route infrastructure

--- app/Domains/SupportInfrastructure/Models/Software.php
+++ app/Domains/SupportInfrastructure/Models/Software.php
@@ -1,5 +1,6 @@
 <?php
-namespace IncadevUns\CoreDomain\Models;
+# namespace IncadevUns\CoreDomain\Models;
+namespace App\Domains\SupportInfrastructure\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
