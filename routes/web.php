a6594f7f2dbfedc60c5d95102c012765ccb62c3a
CRUZ CASTILLO JHOAN ANTONI <116601508+Jhoan2-cpu@users.noreply.github.com>
2025-11-15 19:37:54 -0500

    remove: old module security

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// RUTAS API
-/*
-require base_path('app/Domains/SupportSecurity/routes.php');
-require base_path('app/Domains/Administrator/routes.php');
-require base_path('App/Domains/SupportInfrastructure/routes.php');*/
 
